ccac66446b587cc247c53a6f45e629ff52a28216
Daniel879-blip <happinessd472@gmail.com>
2025-08-06 22:07:32 +0100

    handler.py

--- handler.py
+++ handler.py
@@ -1,11 +1,10 @@
 # handler.py
 
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-def load_data(uploaded_file):
+def load_data(uploaded_file=None):
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
     else:
@@ -13,16 +12,13 @@ def load_data(uploaded_file):
     return df
 
 def preprocess_data(df, test_size=0.2):
-    if "target" not in df.columns:
-        raise ValueError("Dataset must contain a 'target' column.")
-
     X_df = df.drop("target", axis=1)
-    y = df["target"].values
-
+    y = df["target"]
+    
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X_df)
 
     X_train, X_test, y_train, y_test = train_test_split(
         X_scaled, y, test_size=test_size, stratify=y, random_state=42
     )
-    return X_df, X_scaled, X_train, X_test, y_train, y_test, scaler
+    return X_df, scaler, X_train, X_test, y_train, y_test
